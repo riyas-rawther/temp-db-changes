c32d16b484ded513619277d453e7958747b64e1f
Riyas Rawther <riyas@alignminds.com>
2022-08-17 15:09:10 +0530

    fail

--- migration.sql
+++ migration.sql
@@ -1,8 +1,22 @@
+#USE [northwind]
+#GO
+
+#UPDATE [dbo].[Customers]
+#   SET [ContactName] = 'Riyas Rawther'
+#      
+# WHERE [dbo].[Customers].[CustomerID] = 'ALFKI'
+#GO
+
+#FAIL MANUALy
+
+
+
+
 USE [northwind]
 GO
 
 UPDATE [dbo].[Customers]
-   SET [ContactName] = 'Riyas Rawther'
+   SET [CustomerID] = 'Riyas'
       
  WHERE [dbo].[Customers].[CustomerID] = 'ALFKI'
 GO
